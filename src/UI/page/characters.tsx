94368db7b02c1c340a8d5000adad501e6abf308f
0Ieg <obognov@gmail.com>
2023-08-29 23:16:50 +0300

    Working with heroes

--- src/UI/page/characters.tsx
+++ src/UI/page/characters.tsx
@@ -5,11 +5,13 @@ import * as HA from "../../BLL/images/svgstorage";
 
 const HeroesStyled = styled.div`
 width: 100%;
-padding: 0 70px;
-display: grid;
+padding: 0 70px 0 250px;
+display: flex;
+justify-content: space-between;
+/* display: grid;
 grid-template-columns: 80px repeat(4, 1fr);
 grid-auto-rows: 415px;
-gap: 70px;
+gap: 70px; */
 position: relative;
 top: 70px;
 overflow: visible;
@@ -18,10 +20,10 @@ export const Heroes:FC = ()=>{
   return(
     <HeroesStyled>
       <div className="empty"></div>
-      <Hero name='Phoenix'/>
-      <Hero name='Jett'/>
-      <Hero name='Sova'/>
-      <Hero name='Sage'/>
+      <Hero name='Phoenix' country={'United Kingdom'}/>
+      <Hero name='Jett' country={'South Korea'}/>
+      <Hero name='Sova' country={'Russia'}/>
+      <Hero name='Sage' country={'China'}/>
     </HeroesStyled>
   )
 }
@@ -29,18 +31,15 @@ export const Heroes:FC = ()=>{
 
 const HeroStyled = styled.div`
 overflow: visible;
-.background{
-  background-color: green;
-  height: 100%;
-  padding: 0 0 15px 30px;
-  position: relative;
-  overflow: visible;
-  .image{
+height: 415px;
+width: 250px;
+position: relative;
+.image{
     width: 100%;
     height: 100%;
     background-color: var(--color-black);
-  }
-  .abilities{
+}
+.abilities{
     width: calc(100% - 30px);
     height: 60px;
     background-color: var(--color-black);
@@ -49,27 +48,35 @@ overflow: visible;
     z-index: 3;
     bottom: -10px;
     left: 50px;
-  }
-  .name{
+}
+.information{
     position: absolute;
-    top: 13px;
-    left: 13px;
-    width: 26px;
-  }
+    z-index: 4;
+    top: 60px;
+    right: 165px;
+    transform: rotate(90deg);
+    .country{
+      color: var(--color-red);
+      font-size: 22px;
+    }
+    .name{
+      font-size: 63px;
+    }
 }
 `
-const Hero:FC<{name:String}> = (props)=>{
-  const {name} = props
+const Hero:FC<{name:String,country:String}> = (props)=>{
+  const { name, country } = props
   return(
     <HeroStyled>
-      <div className="background">
-        <img className="image" src={heroImage}/>
-        <div className="abilities">
-          <HA.Hero1Ability1/>
-          <HA.Hero1Ability2/>
-          <HA.Hero1Ability3/>
-          <HA.Hero1Ability4/>
-        </div>
+      <img className="image" src={heroImage}/>
+      <div className="abilities">
+        <HA.Hero1Ability1/>
+        <HA.Hero1Ability2/>
+        <HA.Hero1Ability3/>
+        <HA.Hero1Ability4/>
+      </div>
+      <div className="information">
+      <div className="country">{country}</div>
         <div className="name">{name}</div>
       </div>
     </HeroStyled>
