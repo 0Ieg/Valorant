4f6ac83ab0375c7d7812df5c8632812c1092e1fc
0Ieg <obognov@gmail.com>
2023-09-03 23:18:44 +0300

    Revision of the section with the description of abilities

--- src/UI/page/abilities.tsx
+++ src/UI/page/abilities.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { styled } from "styled-components";
+import { Hero4Ability1, Hero4Ability3, Hero4Ability4 } from "../../BLL/images/svgstorage";
 
 const AbilitiesStyled = styled.div`
 display: flex;
@@ -8,15 +9,35 @@ justify-content: space-between;
   height: 150px;
   width: 140px;
   background-color: var(--color-white);
+  position: relative;
+  cursor: pointer;
+  border: 2px solid var(--color-black);
+  svg{
+    position: absolute;
+    width: 80px;
+    fill: var(--color-black);
+    top: 50%;
+    left: 50%;
+    z-index: 6;
+    transform: translateX(-50%) translateY(-50%);
+    transition: fill 0.2s linear;
+  }
+  &:hover svg{
+    fill: var(--color-red);
+  }
 }
 `
 export const Abilities:FC = ()=>{
   return(
     <AbilitiesStyled>
-      <Ability level={'BASIC'} title={'Slow Orb'} description={'Cast out a radianite orb that breaks into a slowing field upon impact with the ground.'}/>
-      <Ability level={'SIGNATURE'} title={'Healing Orb'} description={'Heal an ally or yourself to full health over a few seconds.'}/>
-      <Ability level={'ULTIMATE'} title={'Resurrection'} description={'Target a friendly corpse. After a short delay, revive them with full health.'}/>
-      <div className="right"></div>
+      <Ability level={'BASIC'} title={'Slow Orb'} Image={Hero4Ability1} description={'Cast out a radianite orb that breaks into a slowing field upon impact with the ground.'}/>
+      <Ability level={'SIGNATURE'} title={'Healing Orb'} Image={Hero4Ability3} description={'Heal an ally or yourself to full health over a few seconds.'}/>
+      <Ability level={'ULTIMATE'} title={'Resurrection'} Image={Hero4Ability4} description={'Target a friendly corpse. After a short delay, revive them with full health.'}/>
+      <div className="right">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+        </svg>
+      </div>
     </AbilitiesStyled>
   )
 }
@@ -30,12 +51,14 @@ padding: 10px 20px;
 background-color: var(--color-white);
 border: 2px solid var(--color-black);
 position: relative;
+cursor: pointer;
 .level {
   font-size: 24px;
   font-weight: 700;
   font-family: Montserrat;
   color: var(--color-gray);
   padding-bottom: 10px;
+  transition: color 0.2s linear;
 }
 .title {
   font-size: 20px;
@@ -55,17 +78,30 @@ position: relative;
   bottom: 0;
 }
 .image {
+  position: absolute;
+  right: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+  svg{
+    fill: var(--color-black);
+    width: 80px;
+  }
+}
+&:hover{
+  .level{
+    color: var(--color-red);
+  }
 }
 `
-export const Ability:FC<{level:String,title:String,description:String}> = (props)=>{
-  const {level, title, description} = props
+export const Ability:FC<{level:String,title:String,description:String, Image:any}> = (props)=>{
+  const {level, title, description, Image} = props
   return(
     <AbilityStyled>
       <div className="level">{level}</div>
       <div className="title">{title}</div>
       <div className="description">{description}</div>
       <div className="square"></div>
-      <div className="image"></div>
+      <div className="image"><Image/></div>
     </AbilityStyled>
   )
 }
